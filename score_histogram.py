b96d2cf09ac0dad815df3c4f6b34c3569ed044fb
Aidan Feldman <aidan.feldman@gsa.gov>
2016-12-02 01:09:36 -0500

    histogram score cleanup

--- score_histogram.py
+++ score_histogram.py
@@ -1,3 +1,4 @@
+# fix for not having a DISPLAY
 # http://stackoverflow.com/a/3054314/358804
 import matplotlib
 matplotlib.use('Agg')
@@ -12,7 +13,7 @@ from pathlib import Path
 BUCKET_NAME = 'startup-systems-results'
 FILENAME = 'avg_scores.png'
 
-
+# load the scores
 score_files = glob.glob('/opt/students/*/avg_score.txt')
 scores = [float(Path(score_file).read_text()) for score_file in score_files]
 
@@ -25,12 +26,19 @@ plt.ylabel("Number of students")
 # plt.show()
 # plt.savefig(FILENAME)
 
+# save the image in memory
 # http://stackoverflow.com/questions/31485660/python-uploading-a-plot-from-memory-to-s3-using-matplotlib-and-boto
 img = io.BytesIO()
 plt.savefig(img, format='png')
 img.seek(0)
 
-
+# upload the image to S3
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(BUCKET_NAME)
-bucket.put_object(Key=FILENAME, Body=img, ContentDisposition='inline', ContentType='image/png', ACL='public-read')
+bucket.put_object(
+    ACL='public-read'
+    Body=img,
+    ContentDisposition='inline',
+    ContentType='image/png',
+    Key=FILENAME,
+)
