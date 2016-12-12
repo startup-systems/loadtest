7cd020c9844bcbe486e36037a6f1b705f07f9f04
Aidan Feldman <aidan.feldman@gsa.gov>
2016-12-11 23:46:55 -0500

    grade relative to the baseline score

--- grade.py
+++ grade.py
@@ -3,30 +3,29 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
+BASELINE_FILE = '/Users/aidanfeldman/Desktop/final-project-results/alf239/avg_score.txt'
+BASELINE_SCORE = float(Path(BASELINE_FILE).read_text())
 MAX_GRADE = 100.0
-MIN_GRADE = 75.0
+BASELINE_GRADE = 75.0
 
 score_files = glob.glob('/Users/aidanfeldman/Desktop/final-project-results/*/avg_score.txt')
 scores = [float(Path(score_file).read_text()) for score_file in score_files]
 
-min_score = min(scores)
 max_score = max(scores)
-
 
 # final = actual * N + base
 # 100 = max * N + base
-# 75 = min * N + base
+# 75 = baseline * N + base
 # base = 75 - min * N
 # 100 = max * N + 75 + min * N
 # 25 = N ( max + min )
 
-multiplier = (MAX_GRADE - MIN_GRADE) / (min_score + max_score)
+multiplier = (MAX_GRADE - BASELINE_GRADE) / (BASELINE_SCORE + max_score)
 base = MAX_GRADE - (max_score * multiplier)
 
 scores = [(actual * multiplier + base) for actual in scores]
 
-bins = int((MAX_GRADE - MIN_GRADE) / 5.0)
-plt.hist(scores, bins='auto')
+plt.hist(scores, bins=20)
 
 plt.title("Final project grades")
 plt.xlabel("Student average score")
